1492c44a6ae1b8894aea9060557abae7ea776897
JayDamon <jay.n.damon@gmail.com>
2017-11-10 15:02:17 -0500

    Added new test for prepared statements

--- src/test/java/libraries/orm/utility/DBConnectionTest.java
+++ src/test/java/libraries/orm/utility/DBConnectionTest.java
@@ -1,6 +1,9 @@
 package libraries.orm.utility;
 
 import junit.framework.TestCase;
+import libraries.orm.crud.InsertQuery;
+import libraries.orm.crud.Query;
+import libraries.orm.orm.Table;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -12,9 +15,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import pojo.POJOWithAnnotations;
 
-import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by Jay Damon on 11/10/2017.
@@ -24,20 +27,35 @@ public class DBConnectionTest {
     @InjectMocks private DBConnection dbConnection;
     @Mock private Connection mockConnection;
     @Mock private Statement mockStatement;
+    private PreparedStatement statement;
+    private POJOWithAnnotations pojo;
 
     @BeforeAll
     public void setUp() throws Exception {
-        System.out.println("setUp");
         MockitoAnnotations.initMocks(this);
+        pojo = new POJOWithAnnotations();
+        pojo.setTestString("TestValue");
+        pojo.setTestInt(20);
+        pojo.setTestDouble(30.0D);
+        pojo.setTestDate(new Date(2017, 11, 5));
+        statement = mockConnection.prepareStatement("\"INSERT INTO testTableName (testString, testInt, testDouble, testDate) VALUES(?, ?, ?, ?)");
+//        statement.set
     }
 
     @Test
     public void testMockDBConnection() throws Exception {
-        System.out.println("testMethod");
         Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
         Mockito.when(mockConnection.createStatement().executeUpdate(Mockito.any())).thenReturn(1);
         int value = dbConnection.executeQuery("");
         Assert.assertEquals(value, 1);
         Mockito.verify(mockConnection.createStatement(), Mockito.times(1));
+    }
+
+    //https://stackoverflow.com/questions/28388204/how-to-test-dao-methods-using-mockito TODO stackoverflow post on how to fix test
+    @Test void createSuccessfullPreparedStatement() throws SQLException {
+        Query query = new InsertQuery();
+        PreparedStatement stmt = query.createQuery(mockConnection, new Table(new POJOWithAnnotations()));
+        stmt.setInt(1, 1);
+        Assert.assertEquals(statement, stmt);
     }
 }

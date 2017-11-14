2c738eb0d54c339d5c0167573844fe76ca9057e7
Jay Damon <jay.n.damon@gmail.com>
2017-11-14 07:24:51 -0500

    Added functionality for "createQuery" to add parameters to PreparedStatement.  User can now create the statement and then execute it.

--- src/main/java/libraries/orm/crud/Query.java
+++ src/main/java/libraries/orm/crud/Query.java
@@ -8,98 +8,93 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import libraries.orm.orm.Column;
-import libraries.orm.orm.Crudable;
 import libraries.orm.orm.Table;
 
 public abstract class Query {
     public Query() {
     }
 
-    public PreparedStatement createQuery(Connection conn, Table table) {
-        return PrepareStatement.preparedStatement(conn, this.createQueryString(table));
+    public PreparedStatement createQuery(Connection conn, Table table)
+            throws SQLException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        PreparedStatement statement = PrepareStatement.preparedStatement(conn, this.createQueryString(table));
+        setParameters(table, statement);
+        return statement;
     }
 
-    public void setParameters(Table table, PreparedStatement statement) throws SQLException {
+    public void setParameters(Table table, PreparedStatement statement)
+            throws SQLException, InvocationTargetException, IllegalAccessException, InstantiationException {
         Class<?> clazz = table.getCrudable().getClass();
-        try {
-            Object newInstance = clazz.newInstance();
-            setParameter(table, statement, newInstance);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void setParameter(Table table, PreparedStatement statement, Object newInstance) throws IllegalAccessException, InvocationTargetException, SQLException {
+        Object newInstance = clazz.newInstance();
         for (int i = 0 ; i < table.getColumnList().size() ; i++) {
             Column c = table.getColumnList().get(i);
             Object o = c.getGetterMethod().invoke(newInstance);
-            if (o instanceof Integer) {
-                setParameter(statement, i, (int)o);
-            } else if (o instanceof String) {
-                setParameter(statement, i, (String) o);
-            } else if (o instanceof Short) {
-                setParameter(statement, i, (short)o);
-            } else if (o instanceof BigDecimal) {
-                setParameter(statement, i, (BigDecimal) o);
-            } else if (o instanceof Byte) {
-                setParameter(statement, i, (byte)o);
-            } else if (o instanceof Boolean) {
-                setParameter(statement, i, (boolean)o);
-            } else if (o instanceof Long) {
-                setParameter(statement, i, (long)o);
-            } else if (o instanceof Float) {
-                setParameter(statement, i, (float)o);
-            } else if (o instanceof Double) {
-                setParameter(statement, i, (double)o);
-            } else if (o instanceof Date) {
-                setParameter(statement, i, (Date) o);
-            }
+            setParameterBasedOnType(statement, i, o);
+        }
+    }
+
+    private void setParameterBasedOnType(PreparedStatement statement, int i, Object o) throws SQLException {
+        if (o instanceof Integer) {
+            setStatementParameter(statement, i, (int)o);
+        } else if (o instanceof String) {
+            setStatementParameter(statement, i, (String) o);
+        } else if (o instanceof Short) {
+            setStatementParameter(statement, i, (short)o);
+        } else if (o instanceof BigDecimal) {
+            setStatementParameter(statement, i, (BigDecimal) o);
+        } else if (o instanceof Byte) {
+            setStatementParameter(statement, i, (byte)o);
+        } else if (o instanceof Boolean) {
+            setStatementParameter(statement, i, (boolean)o);
+        } else if (o instanceof Long) {
+            setStatementParameter(statement, i, (long)o);
+        } else if (o instanceof Float) {
+            setStatementParameter(statement, i, (float)o);
+        } else if (o instanceof Double) {
+            setStatementParameter(statement, i, (double)o);
+        } else if (o instanceof Date) {
+            setStatementParameter(statement, i, (Date) o);
         }
     }
 
     abstract String createQueryString(Table table);
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, int value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, int value) throws SQLException {
         stmt.setInt(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, String value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, String value) throws SQLException {
         stmt.setString(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, short value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, short value) throws SQLException {
         stmt.setShort(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, BigDecimal value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, BigDecimal value) throws SQLException {
         stmt.setBigDecimal(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, byte value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, byte value) throws SQLException {
         stmt.setByte(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, boolean value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, boolean value) throws SQLException {
         stmt.setBoolean(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, long value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, long value) throws SQLException {
         stmt.setLong(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, float value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, float value) throws SQLException {
         stmt.setFloat(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, double value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, double value) throws SQLException {
         stmt.setDouble(parameterIndex, value);
     }
 
-    private void setParameter(PreparedStatement stmt, int parameterIndex, Date value) throws SQLException {
+    private void setStatementParameter(PreparedStatement stmt, int parameterIndex, Date value) throws SQLException {
         stmt.setDate(parameterIndex, value);
     }
 
